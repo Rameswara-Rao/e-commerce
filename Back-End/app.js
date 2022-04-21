10554a01795b24504e5ade1b9376076d2510bca1
Rameswara Rao <42777994+Rameswara-Rao@users.noreply.github.com>
2022-04-21 08:33:59 +0530

    DB Connected

---
+++ Back-End/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+const express = require('express')
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var cors = require('cors')
+
+const app = express();
+
+//DB Connection
+const dbUrl = process.env.DBURL;
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+mongoose.connect(dbUrl, connectionParams).then(()=>{
+    console.log("DB CONNECTED");
+})
+.catch((e) => {
+    console.log("Error:" + e);
+})
+
+//Middlewares
+app.use(bodyParser.json);
+app.use(cookieParser());
+app.use(cors());
+
+//PORT
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`app is running at ${port}`);
+})
