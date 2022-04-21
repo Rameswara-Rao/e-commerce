c42b1fc6d4d822b584eb7e3df8a586e2329164d9
Rameswara Rao <42777994+Rameswara-Rao@users.noreply.github.com>
2022-04-21 21:47:48 +0530

    some updates

--- Back-End/controllers/auth.js
+++ Back-End/controllers/auth.js
@@ -1,11 +1,21 @@
+const { validationResult } = require("express-validator");
 const User = require("../models/user");
 
 exports.signup = (req, res) => {
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg
+    })
+  }
+
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err: "Not able to ssave user in DB",
+        err: "Not able to save user in DB",
       });
     }
     res.json({
--- Back-End/routes/auth.js
+++ Back-End/routes/auth.js
@@ -1,10 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, body, validationResult } = require("express-validator");
 
-// const {signout} = require=("../controllers/auth")
-const {signout, signup} =require("../controllers/auth")
+const { signout, signup } = require("../controllers/auth");
 
-router.post("/signup",signup)
-router.get("/signout", signout)
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("must be at least 3 chars long"),
+    check("email").isEmail().withMessage("email id is required"),
+    check("password")
+      .isLength({ min: 5 })
+      .withMessage("please enter password at least 5 char"),
+  ],
+  signup
+);
+router.get("/signout", signout);
 
 module.exports = router;
